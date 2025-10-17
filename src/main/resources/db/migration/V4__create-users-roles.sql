5d6f65f14eafdb0f8aa9c9aa0ac6d2869878473a
Igorsilva123 <igor.silvabrito@outlook.com>
2025-10-17 17:52:01 -0300

    create usuario admin

---
+++ src/main/resources/db/migration/V4__create-users-roles.sql
@@ -0,0 +1,16 @@
+INSERT INTO usuarios(email, senha, nome_completo, nome_usuario, verificado,ativo) VALUES ('rodrigo@email.com', '$2y$10$/8EepTsy2reD5vKLPGn.gOXfFs5b6jjoaaWx/79kvdAfu0U0f4vmS', 'Rodrigo', "rodrigo", 1, 1);
+
+INSERT INTO usuarios(email, senha, nome_completo, nome_usuario, verificado,ativo) VALUES ('joao@email.com', '$2
+y$10$/8EepTsy2reD5vKLPGn.gOXfFs5b6jjoaaWx/79kvdAfu0U0f4vmS', 'João', "jao", 1, 1);
+
+INSERT INTO usuarios(email, senha, nome_completo, nome_usuario, verificado,ativo) VALUES ('ana@email.com', '$2y
+$10$/8EepTsy2reD5vKLPGn.gOXfFs5b6jjoaaWx/79kvdAfu0U0f4vmS', 'Ana', "ana", 1, 1);
+
+INSERT INTO usuarios_perfis(usuario_id, perfil_id) VALUES(1,2);
+
+INSERT INTO usuarios_perfis(usuario_id, perfil_id) VALUES(9,1);
+
+INSERT INTO usuarios_perfis(usuario_id, perfil_id) VALUES(10,3);
+
+
+INSERT INTO usuarios_perfis(usuario_id, perfil_id) VALUES(11,4);
